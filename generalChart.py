e745c211f665aba4de8541259ade28b5f23649cf
LongJeffreySilver <jeffreythedestroyer@gmail.com>
2022-12-22 13:56:33 +0100

    Update of the data for plot

--- generalChart.py
+++ generalChart.py
@@ -1,10 +1,59 @@
 # libraries
+import re
+import subprocess
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
  
+rutaRegistros = "/home/user/Escritorio/Registro_datos" + "/"
+procesoFind = subprocess.run(["find", rutaRegistros, "-type", "f", "-ctime" ,"-20"], capture_output=True,text=True) #Saca los registros de vulnerabilidades de los ultimos 20 dias
+registrosVulnerabilidades = procesoFind.stdout.splitlines()
+
+fileList= list()
+if len(registrosVulnerabilidades) != 0: 
+    for rutaFichero in registrosVulnerabilidades:  #Recorrer los registros vulnerabilidades de los ultimos 20 dias
+        fichero = open(rutaFichero,"r")
+        listaSeveridad = [0,0,0,0] #low[0], medium[1], high[2] y critic[3]
+        for lineaFichero in fichero:
+            #00:00:00:00:00:04;5
+            mac = re.match('[0-9][0-9]:[0-9][0-9]:[0-9][0-9]:[0-9][0-9]:[0-9][0-9]:[0-9][0-9];[0-9]+', lineaFichero)
+            hayMac = bool(mac)
+
+            if hayMac == False:
+                #Meter patron vulnerabilidad para eliminar la cabecera
+                lineaPartida = lineaFichero.split(';')
+                severity = lineaPartida[2]
+            
+                if severity == "low\n":
+                    valor = listaSeveridad[0]
+                    valor = valor +1
+                    listaSeveridad[0] = valor
+                elif severity == "medium\n":
+                    valor = listaSeveridad[1]
+                    valor = valor +1
+                    listaSeveridad[1] = valor
+                elif severity == "high\n":
+                    valor = listaSeveridad[2]
+                    valor = valor +1
+                    listaSeveridad[2] = valor
+                elif severity == "critic\n":
+                    valor = listaSeveridad[3]
+                    valor = valor +1
+                    listaSeveridad[3] = valor
+        fileList.append(listaSeveridad)
+        fichero.close()
+
+
+####################### Grafico #######################
+
 # Make a data frame
-df=pd.DataFrame({'x': range(1,11), 'y1': np.random.randn(10), 'y2': np.random.randn(10)+range(1,11), 'y3': np.random.randn(10)+range(11,21), 'y4': np.random.randn(10)+range(6,16), 'y5': np.random.randn(10)+range(4,14)+(0,0,0,0,0,0,0,-3,-8,-6), 'y6': np.random.randn(10)+range(2,12), 'y7': np.random.randn(10)+range(5,15), 'y8': np.random.randn(10)+range(4,14), 'y9': np.random.randn(10)+range(4,14), 'y10': np.random.randn(10)+range(2,12) })
+'''
+for columna in listaSeveridad:
+    dicColum = {'low': columna[0], 'medium':columna[1], 'high': columna[2], 'critic':columna[3]}
+
+
+    
+df=pd.DataFrame({'x': range(1,11), 'y1': np.random.randn(10), 'y2': np.random.randn(10)+range(1,11), 'y3': np.random.randn(10)+range(11,21), 'y4': np.random.randn(10)+range(6,16)})
  
 # Change the style of plot
 plt.style.use('seaborn-darkgrid')
@@ -31,68 +80,13 @@ plt.show()
 
 
 
-######################################### Otro tipo de gráfico ######################################
+plt.xlim(0,20) #El eje X es el unico fijo
+#for i in range(0,20,1):
+i = 0
+for colum in fileList:
+    i = i+1
+    for j in range(0,len(colum),1):
+        plt.plot(i,colum[j],color="red",linewidth=2.0)
 
-# Libraries
-import matplotlib.pyplot as plt
-import pandas as pd
-from math import pi
- 
-# Set data
-df = pd.DataFrame({
-'group': ['A','B','C','D'],
-'var1': [38, 1.5, 30, 4],
-'var2': [29, 10, 9, 34],
-'var3': [8, 39, 23, 24],
-'var4': [7, 31, 33, 14],
-'var5': [28, 15, 32, 14]
-})
- 
-# ------- PART 1: Define a function that do a plot for one line of the dataset!
- 
-def make_spider( row, title, color):
-
-    # number of variable
-    categories=list(df)[1:]
-    N = len(categories)
-
-    # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
-    angles = [n / float(N) * 2 * pi for n in range(N)]
-    angles += angles[:1]
-
-    # Initialise the spider plot
-    ax = plt.subplot(2,2,row+1, polar=True, )
-
-    # If you want the first axis to be on top:
-    ax.set_theta_offset(pi / 2)
-    ax.set_theta_direction(-1)
-
-    # Draw one axe per variable + add labels labels yet
-    plt.xticks(angles[:-1], categories, color='grey', size=8)
-
-    # Draw ylabels
-    ax.set_rlabel_position(0)
-    plt.yticks([10,20,30], ["10","20","30"], color="grey", size=7)
-    plt.ylim(0,40)
-
-    # Ind1
-    values=df.loc[row].drop('group').values.flatten().tolist()
-    values += values[:1]
-    ax.plot(angles, values, color=color, linewidth=2, linestyle='solid')
-    ax.fill(angles, values, color=color, alpha=0.4)
-
-    # Add a title
-    plt.title(title, size=11, color=color, y=1.1)
-
-    
-# ------- PART 2: Apply the function to all individuals
-# initialize the figure
-my_dpi=96
-plt.figure(figsize=(1000/my_dpi, 1000/my_dpi), dpi=my_dpi)
- 
-# Create a color palette:
-my_palette = plt.cm.get_cmap("Set2", len(df.index))
- 
-# Loop to plot
-for row in range(0, len(df.index)):
-    make_spider( row=row, title='group '+df['group'][row], color=my_palette(row))
+plt.show()
+'''
